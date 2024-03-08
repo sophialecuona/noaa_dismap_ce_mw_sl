e27b5013337d86818ddfd8e26a401993e46c22b9
sophialecuona <sophiamireille@bren.ucsb.edu>
2024-03-08 14:07:14 -0800

    adding gifs to app

--- distribution_app_noaa/ui.R
+++ distribution_app_noaa/ui.R
@@ -3,100 +3,100 @@ header <- dashboardHeader(
   # add title ----
   title = "Economically Important Species Distribution Along California",
   titleWidth = 600
-                  
+
 ) # END dashboardHeader
 
 #........................dashboardSidebar........................
 sidebar <- dashboardSidebar(
-  
+
   # sidebarMenu ----
   sidebarMenu(
-    
+
     menuItem(text = "Overview", tabName = "overview", icon = icon("star")),
     menuItem(text = "Distribution", tabName = "dashboard", icon = icon("fish")),
     menuItem(text = "Revenue", tabName = "more", icon = icon("usd")),
     menuItem(text = "CPUE", tabName = "moremore", icon = icon("fire"))
-    
+
   ) # END sidebarMenu
 ) # END dashboardSidebar
 
 #..........................dashboardBody.........................
 body <- dashboardBody(
-  
+
   # tabItems ----
   tabItems(
-    
+
     # welcome tabItem ----
     tabItem(tabName = "overview",
-            
+
             # left-hand column ----
             column(width = 6,
-                   
+
                    # background info box ----
                    box(width = NULL,
-                       
+
                        title = tagList(icon("water"), strong("Species Distribution over Space and Time")),
                        includeMarkdown("text/intro.md"),
-                       tags$img(src = "cali_map.png", 
+                       tags$img(src = "cali_map.png",
                                 alt = "California Coast.",
                                 style = "max-width: 100%;",
                                 alt = "Map of the California coast from NOAA DisMap."),
                        tags$h6(tags$em("Image Source:", tags$a(href = "https://apps-st.fisheries.noaa.gov/dismap/DisMAP.html", "NOAA DisMap")),
                                style = "text-align: center;")
-                       
+
                    ), # END background info box
-                   
+
             ), # END left-hand column
-            
+
             # right-hand column ----
             column(width = 4,
-                   
+
                    # first fluidRow ----
                    fluidRow(
-                     
+
                      # data source box ----
                      box(width = NULL,
-                         
+
                          title = tagList(icon("table"), strong("Data Citations")),
                          includeMarkdown("text/data_citation.md"),
-                         tags$img(src = "dismap.png", 
+                         tags$img(src = "dismap.png",
                                   alt = "NOAA DisMAP.",
                                   style = "max-width: 50%;"),
                          tags$h6(tags$em("Image Source:", tags$a(href = "https://apps-st.fisheries.noaa.gov/dismap/", "NOAA DisMAP")),
                                  style = "text-align: left;")
                      ) # END data source box
-                     
+
                    ), # END first fluidRow
-                   
+
                    # second fluidRow ----
                    fluidRow(
-                     
+
                      # disclaimer box ----
                      box(width = NULL,
-                         
+
                          title = tagList(icon("triangle-exclamation"), strong("Disclaimer")),
                          includeMarkdown("text/disclaimer.md"),
                          tags$img(src= 'bren_logo.png', height = '60', width ='280')
-                         
+
                      ) # END disclaimer box
-                     
+
                    ) # END second fluidRow
-                   
+
             ) # END right-hand column
-            
-    ), # END welcome tabItem 
-    
+
+    ), # END welcome tabItem
+
     # dashboard tabItem ----
     tabItem(tabName = "dashboard",
-            
+
             # fluidRow ----
             fluidRow(
-              
+
               # input box ----
               box(width = 4, height = 500,
-                  
+
                   title = tags$strong("Adjust depth ranges:"),
-                  
+
                   # sliderInputs ----
                   sliderInput(inputId = "depth_slider_input", label = "Depth (*meters* below SL):",
                               min = min(full_dung_squid_urch1$depth), max = max(full_dung_squid_urch1$depth),
@@ -111,25 +111,49 @@ body <- dashboardBody(
                               min = min(full_dung_squid_urch1$year), max = max(full_dung_squid_urch1$year),
                               value = c(min(full_dung_squid_urch1$year), max(full_dung_squid_urch1$year)),
                               sep = "")
-                  
+
               ), # END input box
-              
-              box(width = 8, height = 500, 
-                  
+
+              box(width = 8, height = 500,
+
                   title = tags$strong("Distribution of Dungeness, Market Squid, and Urchin along the California Coast"),
-                  
+
                   # leaflet output ----
-                  leafletOutput(outputId = "coast_map_output") |> 
+                  leafletOutput(outputId = "coast_map_output") |>
                     withSpinner(type = 1, color = "darkblue")
-                  
-              ) # END leaflet box
-              
+
+              ), # END leaflet box
+
+              # input box ----
+              box(width = 4, height = 350,
+
+                  title = tags$strong("Dungeness Crab (2003 - 2022)"),
+                  tags$img(src= 'dung_distribution.gif', height = '280', width ='280')
+
+              ),# END input box ---
+
+              # input box ----
+              box(width = 4, height = 350,
+
+                  title = tags$strong("Squid Distribution (2003 - 2022)"),
+                  tags$img(src= 'squid_distribution.gif', height = '280', width ='280')
+
+              ), # END input box ---
+
+              # input box ----
+              box(width = 4, height = 350,
+
+                  title = tags$strong("Urchin Distribution (2003 - 2022)"),
+                  tags$img(src= 'urchin_distribution.gif', height = '280', width ='280')
+
+              )# END input box ---
+
             ) # END fluidRow
-            
+
     ) # END dashboard tabItem
-    
+
   ) # END tabItems
-  
+
 ) # END dashboardBody
 
 #..................combine all in dashboardPage..................
--- scratch/distribution_processing.R
+++ scratch/distribution_processing.R
@@ -3,33 +3,126 @@ library(here)
 library(broom)
 library(janitor)
 library(ggplot2)
+library(gganimate)
+library(magick)
+library(gifski)
 
 # Spatial data packages
 library(sf)
 library(tmap)
 library(spatstat)
-library(terra) 
+library(terra)
 
 # CA Counties processing
-ca_counties_raw_sf <- read_sf(here("distribution_app", "data", "ca_counties3", "CA_Counties_TIGER2016.shp"))
+ca_counties_raw_sf <- read_sf(here("data", "ca_counties2", "CA_Counties_TIGER2016.shp"))
 
 ca_counties_raw_sf %>% st_crs()
 ca_counties_raw_sf %>% terra::crs()
 
-ca_counties_sf <- ca_counties_raw_sf %>% 
+ca_counties_sf <- ca_counties_raw_sf %>%
   janitor::clean_names() %>%
   mutate(land_km2 = aland / 1e6) %>%
   select(county = name, land_km2)
 
 ## save processed data to your app's data directory ----
-write_csv(x = ca_counties_sf, file = here::here("distribution_app", "data", "ca_counties_sf.shp"))
+write_csv(x = ca_counties_sf, file = here::here("distribution_app_noaa", "data", "ca_counties_sf.shp"))
 
 # DSC Survey data processing
-full_dung_squid_urch1 <- read_csv(here("distribution_app", "data", "full_dsu.csv"))
+full_dung_squid_urch1 <- read_csv(here("distribution_app_noaa", "data", "full_dsu.csv"))
 
 sf_dsc <- st_as_sf(full_dung_squid_urch1, coords = c("long", "lat"), crs = 4326)
 
 dsc <- st_transform(sf_dsc, st_crs(ca_counties_sf))
 
 ## save processed data to your app's data directory ----
-write_csv(x = dsc, file = here::here("distribution_app", "data", "dsc.shp"))
+write_csv(x = dsc, file = here::here("distribution_app_noaa", "data", "dsc.csv"))
+
+
+# Save by species
+
+## Dungeness
+dung <- dsc %>%
+  filter(species == "dungeness")
+
+## Squid
+squid <- dsc %>%
+  filter(species == "squid")
+
+## Urchin
+urchin <- dsc %>%
+  filter(species == "urchin")
+
+## Dungeness animation
+dung_plot <- ggplot() +
+  geom_sf(data = ca_counties_sf,
+          color = "darkgrey",
+          fill = "grey",
+          size = 1) +
+  geom_sf(data = dung,
+          aes(shape = "square", color = wtcpue),
+          alpha = 0.7,
+          size = 3) +
+  labs(title = "Dungeness Distribution",
+       color = "Catch per unit effort") +
+  guides(shape = "none") +
+  theme_void()
+dung_plot
+
+dung_anim <- dung_plot +
+  transition_time(year) +
+  enter_appear()
+
+anim_save("distribution_app_noaa/www/dung_distribution.gif", animation = dung_anim, nframes = 19)
+
+
+## Squid animate plot
+squid_plot <- ggplot() +
+  geom_sf(data = ca_counties_sf,
+          color = "darkgrey",
+          fill = "grey",
+          size = 1) +
+  geom_sf(data = squid,
+          aes(shape = "square", color = wtcpue),
+          alpha = 0.7,
+          size = 3) +
+  labs(title = "Market Squid Distribution",
+       color = "Catch per unit effort") +
+  guides(shape = "none") +
+  theme_void()
+squid_plot
+
+squid_anim <- squid_plot +
+  geom_text(aes(label = year),
+            data = squid,
+            x = max(ca_counties_sf$geometry$x) - 5,
+            y = min(ca_counties_sf$geometry$y) + 0.1,
+            size = 5,
+            color = "black",
+            show.legend = FALSE,
+            hjust = 1, vjust = 0) +
+  transition_time(year) +
+  enter_appear()
+
+anim_save("distribution_app_noaa/www/squid_distribution.gif", animation = squid_anim, nframes = 19)
+
+## Urchin animation
+urchin_plot <- ggplot() +
+  geom_sf(data = ca_counties_sf,
+          color = "darkgrey",
+          fill = "grey",
+          size = 1) +
+  geom_sf(data = urchin,
+          aes(shape = "square", color = wtcpue),
+          alpha = 0.7,
+          size = 3) +
+  labs(title = "Urchin Distribution",
+       color = "Catch per unit effort") +
+  guides(shape = "none") +
+  theme_void()
+urchin_plot
+
+urchin_anim <- urchin_plot +
+  transition_time(year) +
+  enter_appear()
+
+anim_save("distribution_app_noaa/www/urchin_distribution.gif", animation = urchin_anim, nframes = 19)
